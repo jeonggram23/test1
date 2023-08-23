d316dd2e7c9c405374746e54390fac5c7c350d9d
KangMinju <demo1208@gmail.com>
2023-08-23 17:53:13 +0900

    end

--- posts/urls.py
+++ posts/urls.py
@@ -12,4 +12,11 @@ urlpatterns = [
     path('new/', views.new, name='new'),
     path('create/', views.create, name='create'),
 
+    # Delete
+    path('<int:id>/delete/', views.delete, name='delete'),
+
+    #Update
+    path('<int:id>/edit/', views.edit, name='edit'),
+    path('<int:id>/update/', views.update, name='update')
+
 ]
--- posts/views.py
+++ posts/views.py
@@ -38,6 +38,34 @@ def create(request):
     
     return redirect('posts:detail', id=post.id)
 
+# Delete
+
+def delete(request,id):
+    post = Post.objects.get(id=id)
+    post.delete()
+
+    return redirect('posts:index')
+
+# Update
+def edit(request,id):
+    post = Post.objects.get(id=id)
+    context={
+        'post':post
+    }
+    return render(request, 'edit.html', context)
+
+def update(request,id):
+    title = request.POST.get('title')
+    content = request.POST.get('content')
+
+    post=Post.objects.get(id=id)
+    post.title=title
+    post.content=content
+    post.save()
+
+    return redirect('posts:detail', id=post.id)
+
+
 
 
     
